84e132f922a2b2d29193db403939ee711370a870
senna-lang <asrsena3302@gmail.com>
2024-10-10 21:48:52 +0900

    feat: DBスキーマの修正

--- migrations/0000_nice_scream.sql
+++ migrations/0000_nice_scream.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS "expense_categories" (
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user_finances" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"user_id" integer NOT NULL,
+	"user_id" varchar NOT NULL,
 	"category_id" integer NOT NULL,
 	"amount" numeric(10, 2) NOT NULL,
 	"date" date NOT NULL
@@ -13,15 +13,17 @@ CREATE TABLE IF NOT EXISTS "user_finances" (
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "users" (
 	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" varchar(255) NOT NULL,
 	"username" varchar(50) NOT NULL,
 	"email" varchar(100) NOT NULL,
 	"password_hash" varchar(255) NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_user_id_unique" UNIQUE("user_id"),
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "user_finances" ADD CONSTRAINT "user_finances_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "user_finances" ADD CONSTRAINT "user_finances_user_id_users_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
